dbb0521c85e37751bdd22b3b26b2be50bc9fff2b
Bowie <130021692+Bowie-owo@users.noreply.github.com>
2024-08-23 17:04:35 +0800

    Update Customer.py

--- Customer.py
+++ Customer.py
@@ -3,6 +3,50 @@
 #Cart management - add, remove, mod items
 #Order Tracking - status of orders
 #Product Review - feedback, suggestions
+menu_file = 'menu.txt'
+import json
+import hashlib
+
+
+# Function to hash passwords
+def hash_password(password):
+    return hashlib.md5(password.encode()).hexdigest()
+
+
+def update():
+    # Load users.json data
+    with open('users.json', 'r') as file:
+        users = json.load(file)
+
+    cus_email = input("Enter your email: ")
+    cus_pwd = input("Enter your password: ")
+
+    # Hash the input password for comparison
+    hashed_pwd = hash_password(cus_pwd)
+
+    # Check customer email exists and the password correct
+    if cus_email in users and users[cus_email]['password'] == hashed_pwd:
+        new_email = input("Enter new email: ")
+        new_pwd = input("Enter new password: ")
+
+        # Hash the new password
+        new_h_pwd = hash_password(new_pwd)
+
+        # Update user data
+        del users[cus_email]
+        users[new_email] = {
+            "email": new_email,
+            "password": new_h_pwd
+        }
+
+        # Write the updated users data back to the file
+        with open('users.json', 'w') as file:
+            json.dump(users, file, indent=4)
+
+        print("Your information was updated successfully.")
+    else:
+        print("No matching account.")
+        
 def cart():
     with open('menu.txt','r') as file:
         lines = file.readlines()
