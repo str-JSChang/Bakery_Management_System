3841b9a73409203b50b7f66dbaa943debe2b94c0
str-JSChang <js.chang.work@gmail.com>
2024-09-10 20:52:22 +0800

    Update manager.py

--- manager.py
+++ manager.py
@@ -1,19 +1,30 @@
-import json
-import os
+import csv
 
-# Open the JSON File
-def load_users():
-    # Without os.path.join, it would not join the current script path together with users.json
-    file_path = os.path.join(os.path.dirname(__file__), 'users.json')
-    try:
-        with open(file_path, 'r') as f:
-            return json.load(f)
-    except FileNotFoundError:
-        print("File not found. Returning an empty dictionary.")
-        return {}
 
-users = load_users()
-print(users)
+def managerPage():
+    print("----------Manager's page----------")
+    print("1. Manage System Credentials")
+    print("2. Manage Orders")
+    print("3. Manage Company Financials")
+    print("4. Manage Inventory")
+    print("5. View Customer Feedback")
+    
+# System Credentials page
+def viewCredentials():
+    print("")
 
-# Explaination of special variable __file__ is a special/method/variable to return the current path
-#print(__file__)
+# Orders detail page
+def viewOrders():
+    pass
+
+# Financials page
+def viewFinancials():
+    pass
+
+# Inventory page
+def viewInventory():
+    pass
+
+# Customer feedback
+def viewCus_feedback():
+    pass
