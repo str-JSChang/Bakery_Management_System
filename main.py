c378875eb95a62bcd53cd078883246349211d6bb
str-JSChang <js.chang.work@gmail.com>
2024-09-26 05:00:40 +0800

    REWRITE THE WHOLE MAIN PY CODE STRUCTURE

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import csv
 import hashlib
 
 #banner
@@ -10,31 +9,36 @@ def print_banner():
     print("                       /___/                        /___/                             /___/                   ")
     print("")
 
-# Main Menu
-def display_main_menu():
-    while True: 
-        print("Welcome to Bakery Management System")
-        print("1. Login. ")
-        print("2. Signup. ")
-        print("3. Exit the program. ")
-        while True:
-            try:
-                choice = int(input("Please enter your selection (1-3): "))  # REPROMPT WITH ERRORS EXCEPTION HANDLING
-                if choice in [1, 2, 3]:
-                    break
-                else:
-                    print("Please enter a valid option between 1 to 3.")
-            except ValueError:
-                print("Invalid Input. Error occurs, Please enter NUMBER ONLY between 1 and 3.")
-        
-        if choice == 1:
-            login()
-        elif choice == 2:
-            signup()
-        elif choice == 3:
-            print("Exiting program...")
-            exit()
+def login():
+    print("--------Login Page--------")
+    username = input("Please enter your username: ")
+    pwd = input("Please enter your password: ")
 
+    try:
+        with open("user_data.csv", "r") as file:
+            for line in file:
+                fields = line.strip().split(",")
+                if fields[2] == username and fields[3] == hashlib.md5(pwd.encode()).hexdigest():
+                    print(f"Login successful! Welcome, {fields[0]}.")
+                    if fields[4] == "manager":
+                        # Proceed to customer functions
+                        pass
+                    elif fields[4] == "customer":
+                        # Proceed to manager functions
+                        pass
+                    elif fields[4] == "cashier":
+                        # Proceed to baker functions
+                        pass
+                    elif fields[4] == "baker":
+                        # Proceed to cashier functions
+                        pass
+                    else:
+                        print("Invalid user role.")
+                    return
+            print("Account not found or incorrect password.")
+    except FileNotFoundError:
+        print("Error: 'user_data.csv' file not found.")
+        print("PLEASE CONTACT ADMINISTRATOR IMMEDIATELY")
 
 def validate_email():
     while True:
@@ -46,49 +50,12 @@ def validate_email():
         else:
             print("Invalid email. Please use a valid domain. eg: (@gmail.com).")
 
-
-# Manager Login
-# Check Line112 - 2023-08-23
-def ManagerLogin():
-    print("To register as staff, your account must be 'Manager' to register staff account.")
-    manager_username = input("Please enter Manager username: ")
-    manager_password = input("Please enter Manager password: ")
-    confirmManager_password = input("Confirm Manager password: ")
-
-    # confirm manager password
-    if confirmManager_password == manager_password:
-        try:
-            # Create a open file object, together with DictReader, create a Reader object using DictReader to find the header, and return the key values.
-            with open("manager_acc.csv", "r", newline='') as file:
-                reader = csv.DictReader(file)
-                existing_manager_acc = [row for row in reader]
-
-            # Using "List Comprehension" instead of building global empty list in the beginning.
-            manager_accounts = [account for account in existing_manager_acc if account['Username'] == manager_username]
-
-            if manager_accounts:
-                manager_stored_password = manager_accounts[0]['Password']
-                hashing_manager_password = hashlib.md5(manager_password.encode()).hexdigest()
-                if manager_stored_password == hashing_manager_password:
-                    print(f"Welcome back, {manager_accounts[0]['Name']}")
-                else:
-                    print("Invalid password")
-            else:
-                print("Manager username not found")
-
-        except FileNotFoundError:
-            print("ERROR, NO MANAGER ACCOUNT EXISTS IN THE SYSTEM, PLEASE CONTACT SYSTEM ADMINISTRATOR IMMEDIATELY")
-            signup()
-    else:
-        print("Passwords do not match")
-
-# Sign Up system
 def signup():
     print("----------Signup page----------")
     print("1. Register as Customer")
     print("2. Register as Staff")
     print("3. Exit")
-    
+
     while True:
         try:
             signupOption = int(input("Please enter your selection (1-3):  "))
@@ -97,83 +64,105 @@ def signup():
             else:
                 print("Please enter a valid option between 1 to 3. ")
         except ValueError:
-            print("Invalid Input. Error occurs, Please enter NUMBER ONLY between 1 and 3. ")
-
+            print("Invalid Input. Error occurs, Please enter NUMBER ONLY between 1 and 3.")
 
     if signupOption == 1:
-        name = input("Please enter your name: ")
-        email = validate_email()
-        username = input("Please enter your username: ")
-        pwd = input("Please enter your password: ")
-        confirmPwd = input("Confirm password: ")
-        
-        if confirmPwd == pwd:
-            hashed_pwd = hashlib.md5(pwd.encode()).hexdigest()
-            
-            # Validate the existing account
-            with open("customer_data.csv","r", newline='') as file:
-                reader = csv.DictReader(file)
-                existing_emails = [row['Email'] for row in reader]
-                existing_usernames = [row['Username'] for row in reader]
-            if email in existing_emails or  username in existing_usernames:
-                print("The account has existed.")
-                return
-
-            # Append new row of data.
-            with open("customer_data.csv", "a", newline='') as file:
-                writer = csv.writer(file)
-                writer.writerow([name, email, username, hashed_pwd, "customer"])
-            print("You have registered successfully.")
-        else:
-            print("The passwords doesn't match. \n")
-
-    if signupOption == 2:
-        ManagerLogin()
-        # Do I need to make managerlogin as a function? or no need, just proceduring it as a part of signing up staff?
-
-    if signupOption == 3:
+        register_customer()
+    elif signupOption == 2:
+        register_staff()
+    elif signupOption == 3:
         exit()
-        
 
-def staffSignup():
-    name = input("Please enter your name:")
-    staffid = input("Please enter your Staff ID that you want to register: ")
+def register_customer():
+    name = input("Please enter your name: ")
+    email = validate_email()
     username = input("Please enter your username: ")
-    email = validate_email
     pwd = input("Please enter your password: ")
-    confirmPwd = input("Please confirm your password: ")
-    
+    confirmPwd = input("Confirm password: ")
+
     if confirmPwd == pwd:
         hashed_pwd = hashlib.md5(pwd.encode()).hexdigest()
-
-# Login System
-def login():
-    print("--------Login Page--------")
-    username = input("Please enter your username: ")
-    pwd = input("Please enter your password: ")
-
-    try:
-        with open("customer_data.csv", "r", newline='') as file:
-            reader = csv.DictReader(file)
-            for row in reader:
-                if row['Username'] == username:
-                    if row['Password'] == hashlib.md5(pwd.encode()).hexdigest():
-                        print(f"Login successful! Welcome, {row.get('Name', username)}.")
+        try:
+            with open("user_data.csv", "r") as file:
+                for line in file:
+                    fields = line.strip().split(",")
+                    if fields[1] == email or fields[2] == username:
+                        print("The account has already existed.")
                         return
-                    else:
-                        print("Account exists, but wrong password.")
+        except FileNotFoundError:
+            print("Error: File not found.")
+            print("PLEASE CONTACT ADMINISTRATOR IMMEDIATELY")
+
+        with open("user_data.csv", "a") as file:
+            file.write(f"{name},{email},{username},{hashed_pwd},customer\n")
+        print("You have registered successfully.")
+    else:
+        print("The passwords don't match.")
+
+def register_staff():
+    print("To register staff account, your account must be 'Manager' to register staff account.")
+    manager_username = input("Please enter Manager username: ")
+    manager_password = input("Please enter Manager password: ")
+    confirmManager_password = input("Confirm Manager password: ")
+
+    if confirmManager_password == manager_password:
+        try:
+            with open("user_data.csv", "r") as file:
+                for line in file:
+                    fields = line.strip().split(",")
+                    if fields[2] == manager_username and fields[3] == hashlib.md5(manager_password.encode()).hexdigest() and fields[4] == "manager":
+                        staff_name = input("Please enter the staff's name: ")
+                        staff_id = input("Please enter the staff's ID: ")
+                        staff_username = input("Please enter the staff's username: ")
+                        staff_email = validate_email()
+                        staff_role = input("Please enter the staff's role: ")
+                        staff_pwd = input("Please enter the staff's password: ")
+                        confirmStaff_pwd = input("Confirm the staff's password: ")
+
+                        # check staff account exist
+                        for line in file:
+                            fields = line.strip().split(",")
+                            if fields[2] == staff_username or fields[1] == staff_email or fields[5] == staff_id:
+                                print("This staff account already exists")
+                                return
+
+                        if confirmStaff_pwd == staff_pwd:
+                            hashed_pwd = hashlib.md5(staff_pwd.encode()).hexdigest()
+                            with open("user_data.csv", "a") as file:
+                                file.write(f"{staff_name},{staff_email},{staff_username},{hashed_pwd},{staff_role},{staff_id}\n")
+                            print("You have registered the staff successfully.")
+                        else:
+                            print("The passwords don't match.")
                         return
-            print("Account not found.\n", "Please make sure your account is registered or ensure your username and password are correct.")
-    except FileNotFoundError:
-        print("Error: 'customer_data.csv' file not found.")
-        print("PLEASE CONTACT ADMINISTRATOR IMMEDIATELY")
+                print("Invalid manager credentials.")
+        except FileNotFoundError:
+            print("Error: File not found.")
+            print("PLEASE CONTACT ADMINISTRATOR IMMEDIATELY")
+    else:
+        print("The passwords don't match.")
 
-
-# Main program
 def main():
     print_banner()
-    display_main_menu()
 
+    while True:
+        print("Welcome to Bakery Management System")
+        print("1. Login")
+        print("2. Signup")
+        print("3. Exit the program")
+
+        try:
+            choice = int(input("Please enter your selection (1-3): "))
+            if choice == 1:
+                login()
+            elif choice == 2:
+                signup()
+            elif choice == 3:
+                print("Exiting program...")
+                exit()
+            else:
+                print("Please enter a valid option between 1 to 3.")
+        except ValueError:
+            print("Invalid Input. Error occurs, Please enter NUMBER ONLY between 1 and 3.")
 
 if __name__ == "__main__":
     main()
